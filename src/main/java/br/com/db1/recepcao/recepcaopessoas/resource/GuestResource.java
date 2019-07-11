bf321b18bfac56387d6d520a0a2dbdbf69d5428c
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 12:07:13 -0300

    Method post added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/GuestResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/GuestResource.java
@@ -1,7 +1,10 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.GuestDTO;
 import br.com.db1.recepcao.recepcaopessoas.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ public class GuestResource {
 
     @Autowired
     private GuestService guestService;
+
+    @PostMapping
+    public GuestDTO post(@RequestBody GuestDTO body) {
+        return guestService.save(body);
+    }
 
 }
