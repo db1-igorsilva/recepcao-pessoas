98cdc45b50a4731c4c762d44b8264048f032b815
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 11:53:16 -0300

    GuestRepository returns changed from GuestDTO to Guest

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepository.java
@@ -1,6 +1,5 @@
 package br.com.db1.recepcao.recepcaopessoas.repository;
 
-import br.com.db1.recepcao.recepcaopessoas.domain.dto.GuestDTO;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +9,7 @@ import java.util.UUID;
 
 public interface GuestRepository extends JpaRepository<Guest, UUID> {
 
-    GuestDTO findByName(String name);
-    List<GuestDTO> findByRelationshipType(RelationshipType relationshipType);
+    Guest findByName(String name);
+    List<Guest> findByRelationshipType(RelationshipType relationshipType);
 
 }
