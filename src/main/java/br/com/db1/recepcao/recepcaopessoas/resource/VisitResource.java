fa3412444e600f3e2e536d2eab57c2e0142bbb3a
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:50:46 -0300

    Method getByDate added on VisitResource

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
@@ -1,13 +1,21 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.VisitDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/visit")
 public class VisitResource {
 
-    
+    @GetMapping(value = "/getByDate/{date}")
+    public List<VisitDTO> getByDate(@RequestParam("date") LocalDate date) {
+//        
+    }
 
 }
