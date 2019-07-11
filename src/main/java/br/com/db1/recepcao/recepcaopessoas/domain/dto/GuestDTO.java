59b7f8031533ae21f3ea9e97e61a60d089a3fadb
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:37:37 -0300

    Constructor added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/GuestDTO.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/GuestDTO.java
@@ -13,4 +13,13 @@ public class GuestDTO implements Serializable {
     private String name;
     private RelationshipType relationshipType;
 
+    // CONSTRUCTOR
+    
+    public GuestDTO(UUID id, String name,
+                    RelationshipType relationshipType) {
+        this.id = id;
+        this.name = name;
+        this.relationshipType = relationshipType;
+    }
+
 }
