143de1218015b8f6868a3f0f6d2fc8c6f6abe9c1
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:33:39 -0300

    Attributes added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/VisitDTO.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/VisitDTO.java
@@ -1,9 +1,23 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.dto;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
+
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
 
 public class VisitDTO implements Serializable {
 
-    
+    public static final long serialVersionUID = 23L;
+
+    private UUID id;
+    private LocalDate date;
+    private LocalTime presentationStartTime;
+    private LocalTime presentationEndTime;
+    private Guest guest;
+    private List<String> persons;
+    private String welcomeText;
 
 }
