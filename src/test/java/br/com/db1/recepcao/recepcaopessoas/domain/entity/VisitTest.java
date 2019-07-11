21065ec13b3a1e416a2d549f9c1d925874f1a1c3
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 13:03:51 -0300

    presentationStartTime and presentationEndTime fixed

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/VisitTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/VisitTest.java
@@ -14,8 +14,8 @@ import static org.junit.Assert.*;
 public class VisitTest {
 
     private final static LocalDate date = LocalDate.parse("3000-12-31");
-    private final static LocalTime presentationStartTime = LocalTime.parse("12");
-    private final static LocalTime presentationEndTime = LocalTime.parse("14:30");
+    private final static LocalTime presentationStartTime = LocalTime.parse("12:00:00");
+    private final static LocalTime presentationEndTime = LocalTime.parse("14:30:00");
     private final static Guest guest = new Guest.GuestBuilder()
             .guestName("Company")
             .relatedBy(RelationshipType.OTHER)
