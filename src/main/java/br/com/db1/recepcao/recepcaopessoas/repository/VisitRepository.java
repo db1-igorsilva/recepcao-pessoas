3d2407ba8f915966035408112f68c453e1e03af5
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:48:44 -0300

    Fixed VisitRepository returns

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitRepository.java
@@ -6,11 +6,12 @@ import br.com.db1.recepcao.recepcaopessoas.domain.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface VisitRepository extends JpaRepository<Visit, UUID> {
 
-    VisitDTO findByDate(LocalDate date);
-    VisitDTO findByGuest(Guest guest);
+    List<VisitDTO> findByDate(LocalDate date);
+    List<VisitDTO> findByGuest(Guest guest);
 
 }
