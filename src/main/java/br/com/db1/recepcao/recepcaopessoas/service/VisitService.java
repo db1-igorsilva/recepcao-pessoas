4c9c4e9b5a7cb3b6d1ac585a2dd705d3bb69047d
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:55:21 -0300

    @Autowired VisitRepository added to VisitService

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitService.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitService.java
@@ -1,10 +1,13 @@
 package br.com.db1.recepcao.recepcaopessoas.service;
 
+import br.com.db1.recepcao.recepcaopessoas.repository.VisitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VisitService {
 
-    
+    @Autowired
+    private VisitRepository visitRepository;
 
 }
