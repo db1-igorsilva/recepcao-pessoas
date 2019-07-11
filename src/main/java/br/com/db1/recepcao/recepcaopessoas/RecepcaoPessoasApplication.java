d55851fc89ce1a22a66f226d297e1f39e30e6fab
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 08:52:14 -0300

    Projeto recepcao-pessoas criado.

---
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/RecepcaoPessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.db1.recepcao.recepcaopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepcaoPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepcaoPessoasApplication.class, args);
+	}
+
+}
