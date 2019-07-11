219c7f72eecf1625b5e79b6dbd49f74f35ff7c3a
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 09:37:53 -0300

    Attribute id added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Visit.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Visit.java
@@ -1,12 +1,14 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name = "visit")
 public class Visit {
 
-    
+    @Id
+    @GeneratedValue(GenerationType.IDENTITY)
+    private UUID id;
 
 }
