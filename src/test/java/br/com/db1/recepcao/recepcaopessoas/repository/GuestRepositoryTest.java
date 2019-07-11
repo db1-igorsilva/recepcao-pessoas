0c6e6bb1e3598b9dff001ae7e5b399a8eca0e035
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 13:06:13 -0300

    @Autowired GuestRepository added

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepositoryTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepositoryTest.java
@@ -1,6 +1,7 @@
 package br.com.db1.recepcao.recepcaopessoas.repository;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GuestRepositoryTest {
 
-    
+    @Autowired
+    private GuestRepository guestRepository;
 
 }
