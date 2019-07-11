4e9fcbe38e06ff4955ec91d7624b73ffcfd8b135
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 12:05:52 -0300

    @Autowired GuestServiced added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/GuestResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/GuestResource.java
@@ -1,5 +1,7 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
+import br.com.db1.recepcao.recepcaopessoas.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/guest")
 public class GuestResource {
 
-    
+    @Autowired
+    private GuestService guestService;
 
 }
