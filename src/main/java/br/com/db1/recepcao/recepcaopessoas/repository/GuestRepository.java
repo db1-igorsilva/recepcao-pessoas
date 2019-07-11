d78b591b90df0a774c6a7d858462f0b6064957a8
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:23:04 -0300

    GuestRepository completed

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepository.java
@@ -1,12 +1,16 @@
 package br.com.db1.recepcao.recepcaopessoas.repository;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.GuestDTO;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GuestRepository extends JpaRepository<Guest, UUID> {
 
-    
+    GuestDTO findByName(String name);
+    List<GuestDTO> findByRelationshipType(RelationshipType relationshipType);
 
 }
