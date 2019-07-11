ac453ebeac465192e029402341da557ac23dccca
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:56:28 -0300

    Method getByDate added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitService.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitService.java
@@ -1,13 +1,21 @@
 package br.com.db1.recepcao.recepcaopessoas.service;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.VisitDTO;
 import br.com.db1.recepcao.recepcaopessoas.repository.VisitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class VisitService {
 
     @Autowired
     private VisitRepository visitRepository;
+
+    public List<VisitDTO> getByDate(LocalDate date) {
+        return visitRepository.findByDate(date);
+    }
 
 }
