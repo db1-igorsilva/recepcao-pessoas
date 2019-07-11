305f96d1dbc6cf42db33abc4ce3f1439e30d5086
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 09:20:58 -0300

    Attribute id added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Guest.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Guest.java
@@ -1,12 +1,19 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.entity;
 
+import org.springframework.data.annotation.Id;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Entity
 @Table(name = "guest")
 public class Guest {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
 
 }
