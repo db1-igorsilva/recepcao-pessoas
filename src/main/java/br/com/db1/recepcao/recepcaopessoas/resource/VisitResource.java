286b33a5c307d91ef4c07084dc1de69a43be2bfb
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:45:56 -0300

    Annotations @RestController and @RequestMapping added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
@@ -1,4 +1,13 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/visit")
 public class VisitResource {
+
+    
+
 }
