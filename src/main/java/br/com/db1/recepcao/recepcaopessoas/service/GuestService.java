273a2238d137e1b523a27777017824268a2f1f8d
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 11:49:33 -0300

    Method save added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/service/GuestService.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/GuestService.java
@@ -1,5 +1,7 @@
 package br.com.db1.recepcao.recepcaopessoas.service;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.GuestDTO;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,15 @@ public class GuestService {
 
     @Autowired
     private GuestRepository guestRepository;
+
+    // CREATE
+
+    public GuestDTO save(GuestDTO dto) {
+        Guest guest = new Guest.GuestBuilder()
+                .guestName(dto.getName())
+                .relatedBy(dto.getRelationshipType())
+                .build();
+        return guestToDto(guestRepository.save(guest));
+    }
 
 }
