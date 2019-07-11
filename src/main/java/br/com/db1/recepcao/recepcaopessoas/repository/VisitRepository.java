6244bff2b77a0ac9a05d7af8c35c4d77e65a74ac
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:23:18 -0300

    VisitRepository completed

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitRepository.java
@@ -1,12 +1,16 @@
 package br.com.db1.recepcao.recepcaopessoas.repository;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.VisitDTO;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface VisitRepository extends JpaRepository<Visit, UUID> {
 
-
+    VisitDTO findByDate(LocalDate date);
+    VisitDTO findByGuest(Guest guest);
 
 }
