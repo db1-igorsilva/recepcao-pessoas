5e7f33f70b0645db3ac917b7fd5b14f00d92d962
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 13:33:50 -0300

    final static removed from the attributes

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/VisitTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/VisitTest.java
@@ -13,15 +13,15 @@ import static org.junit.Assert.*;
 
 public class VisitTest {
 
-    private final static LocalDate date = LocalDate.parse("3000-12-31");
-    private final static LocalTime presentationStartTime = LocalTime.parse("12:00:00");
-    private final static LocalTime presentationEndTime = LocalTime.parse("14:30:00");
-    private final static Guest guest = new Guest.GuestBuilder()
+    private LocalDate date = LocalDate.parse("3000-12-31");
+    private LocalTime presentationStartTime = LocalTime.parse("12:00:00");
+    private LocalTime presentationEndTime = LocalTime.parse("14:30:00");
+    private Guest guest = new Guest.GuestBuilder()
             .guestName("Company")
             .relatedBy(RelationshipType.OTHER)
             .build();
-    private final static List<String> persons = asList("First Guest", "Second Guest", "Third Guest", "Last Guest");
-    private final static String welcomeText = "W3lcome, Company guests";
+    private List<String> persons = asList("First Guest", "Second Guest", "Third Guest", "Last Guest");
+    private String welcomeText = "W3lcome, Company guests";
     private Visit visit;
 
     @Before
