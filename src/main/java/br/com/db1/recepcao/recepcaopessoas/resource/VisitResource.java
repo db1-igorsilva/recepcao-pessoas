5e1ff027e389c3ece2756c292abd1d2cb8e71cd1
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 11:14:32 -0300

    Method getByGuest changed: id is now the parameter

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
@@ -1,6 +1,7 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
 import br.com.db1.recepcao.recepcaopessoas.domain.dto.VisitDTO;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.service.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/visit")
@@ -21,6 +23,11 @@ public class VisitResource {
     @GetMapping(value = "/getByDate/{date}")
     public List<VisitDTO> getByDate(@RequestParam("date") LocalDate date) {
         return visitService.getByDate(date);
+    }
+
+    @GetMapping(value = "/getByGuest/{guest}")
+    public List<VisitDTO> getByGuest(@RequestParam("guest") UUID id) {
+        return visitService.getByGuest(id);
     }
 
 }
