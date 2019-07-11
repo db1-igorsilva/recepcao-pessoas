7b3fe5e2b3dce2babc3b381843b46b8cce4821bd
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 09:19:09 -0300

    @Entity and @Table annotations added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Guest.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Guest.java
@@ -1,4 +1,12 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "guest")
 public class Guest {
+
+
+
 }
