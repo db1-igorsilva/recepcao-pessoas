169e423d06fc8630e8229250a8c8012496790d96
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 12:41:35 -0300

    @Test mustCreateOneGuest added

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/GuestTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/GuestTest.java
@@ -1,4 +1,21 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.entity;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class GuestTest {
+    
+    private final static String name = "Guest Name";
+    private final static RelationshipType relationship = RelationshipType.CLIENT;
+
+    @Test
+    public void mustCreateOneGuest() {
+        Guest guest = new Guest.GuestBuilder()
+                .guestName(name)
+                .relatedBy(relationship)
+                .build();
+        assertNotNull(guest);
+    }
+
 }
