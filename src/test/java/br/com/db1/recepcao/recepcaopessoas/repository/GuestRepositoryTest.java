d752e9618cef6dbcb9609a9964ab632d1bfba9ba
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 13:17:13 -0300

    Method mustReturnOneGuest added

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepositoryTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepositoryTest.java
@@ -2,11 +2,15 @@ package br.com.db1.recepcao.recepcaopessoas.repository;
 
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,5 +22,13 @@ public class GuestRepositoryTest {
             .guestName(guestName)
             .relatedBy(RelationshipType.UNIVERSITY)
             .build();
+
+    @Test
+    public void mustReturnOneGuest() {
+        when(fakeGuestRepository.findByName(guestName)).thenReturn(universityGuest);
+        Guest guest = fakeGuestRepository.findByName(guestName);
+        assertThat(guest, not(null));
+        assertEquals(universityGuest, guest);
+    }
 
 }
