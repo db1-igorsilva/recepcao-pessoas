8eb04c19fd8bde211dba61546a3f07e230e7ae46
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:34:28 -0300

    Constructor and Getters and Setters added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/VisitDTO.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/VisitDTO.java
@@ -20,4 +20,79 @@ public class VisitDTO implements Serializable {
     private List<String> persons;
     private String welcomeText;
 
+    // CONSTRUCTOR
+
+    public VisitDTO(UUID id, LocalDate date,
+                    LocalTime presentationStartTime,
+                    LocalTime presentationEndTime,
+                    Guest guest,
+                    List<String> persons,
+                    String welcomeText) {
+        setId(id);
+        setDate(date);
+        setPresentationStartTime(presentationStartTime);
+        setPresentationEndTime(presentationEndTime);
+        setGuest(guest);
+        setPersons(persons);
+        setWelcomeText(welcomeText);
+    }
+
+    // GETTERS AND SETTERS
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getPresentationStartTime() {
+        return presentationStartTime;
+    }
+
+    public void setPresentationStartTime(LocalTime presentationStartTime) {
+        this.presentationStartTime = presentationStartTime;
+    }
+
+    public LocalTime getPresentationEndTime() {
+        return presentationEndTime;
+    }
+
+    public void setPresentationEndTime(LocalTime presentationEndTime) {
+        this.presentationEndTime = presentationEndTime;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public List<String> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<String> persons) {
+        this.persons = persons;
+    }
+
+    public String getWelcomeText() {
+        return welcomeText;
+    }
+
+    public void setWelcomeText(String welcomeText) {
+        this.welcomeText = welcomeText;
+    }
+
 }
