46981b8416ab115b60758274136bbc510dfa4431
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 12:46:08 -0300

    VisitTest created

---
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/VisitTest.java
@@ -0,0 +1,9 @@
+package br.com.db1.recepcao.recepcaopessoas.domain.entity;
+
+import java.time.LocalDate;
+
+public class VisitTest {
+
+    private final static LocalDate date = LocalDate.parse("3000-12-31");
+
+}
