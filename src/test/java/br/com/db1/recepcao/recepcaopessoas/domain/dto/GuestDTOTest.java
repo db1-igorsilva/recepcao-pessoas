3da3708fc45f3c5240fa172381faa01a008cba1f
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 13:38:18 -0300

    @Test mustCreateOneGuestDTO added

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/GuestDTOTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/GuestDTOTest.java
@@ -1,4 +1,24 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.dto;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class GuestDTOTest {
+
+    private String name = "Guest Name";
+    private RelationshipType relationship = RelationshipType.OTHER;
+
+    @Test
+    public void mustCreateOneGuestDTO() {
+        GuestDTO guestDTO = new GuestDTO(UUID.randomUUID(),
+                name, relationship);
+        assertNotNull(guestDTO);
+        assertEquals(name, guestDTO.getName());
+    }
+
 }
