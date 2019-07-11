142dd23673c2bae32a5c0201bae7f088dd6f69d3
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 10:36:44 -0300

    GuestDTO attributes added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/GuestDTO.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/GuestDTO.java
@@ -1,9 +1,16 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.dto;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
+
 import java.io.Serializable;
+import java.util.UUID;
 
 public class GuestDTO implements Serializable {
 
-    
+    public static final long serialVersionUID = 23L;
+
+    private UUID id;
+    private String name;
+    private RelationshipType relationshipType;
 
 }
