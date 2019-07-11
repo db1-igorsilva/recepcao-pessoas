a1f46b9871c6f3313693f16363805233b9c88270
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 11:46:09 -0300

    @Autowired GuestRepository

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/service/GuestService.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/GuestService.java
@@ -1,10 +1,13 @@
 package br.com.db1.recepcao.recepcaopessoas.service;
 
+import br.com.db1.recepcao.recepcaopessoas.repository.GuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GuestService {
 
-
+    @Autowired
+    private GuestRepository guestRepository;
 
 }
