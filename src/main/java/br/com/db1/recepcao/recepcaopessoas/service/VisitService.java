df9c8a4101278b18e64e7393c173b03a75af66cd
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 11:05:50 -0300

    Method visitToDto added and getByGuest fixed to convert all visits to VisitDTO

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitService.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitService.java
@@ -2,12 +2,14 @@ package br.com.db1.recepcao.recepcaopessoas.service;
 
 import br.com.db1.recepcao.recepcaopessoas.domain.dto.VisitDTO;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Visit;
 import br.com.db1.recepcao.recepcaopessoas.repository.VisitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VisitService {
@@ -20,7 +22,17 @@ public class VisitService {
     }
 
     public List<VisitDTO> getByGuest(Guest guest) {
-        return visitRepository.findByGuest(guest);
+        List<Visit> visitsFromTheGuest = visitRepository.findByGuest(guest);
+        List<VisitDTO> visitsDto = visitsFromTheGuest.stream()
+                .map(visit -> visitToDto(visit))
+                .collect(Collectors.toList());
+        return visitsDto;
+    }
+
+    private VisitDTO visitToDto(Visit visit) {
+        return new VisitDTO(visit.getId(), visit.getDate(),
+                visit.getPresentationStartTime(), visit.getPresentationEndTime(),
+                visit.getGuest(), visit.getPersons(), visit.getWelcomeText());
     }
 
 }
