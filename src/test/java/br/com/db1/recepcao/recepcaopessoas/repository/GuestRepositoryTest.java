e1290fecab4b070047aea15273128cd780067a82
db1-igorsilva <igor.silva@db1.com.br>
2019-07-11 13:16:27 -0300

    GuestRepositoryTest's attributes added

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepositoryTest.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepositoryTest.java
@@ -1,15 +1,22 @@
 package br.com.db1.recepcao.recepcaopessoas.repository;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.mock;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GuestRepositoryTest {
 
-    @Autowired
-    private GuestRepository guestRepository;
+    private final static String guestName = "University";
+    private GuestRepository fakeGuestRepository = mock(GuestRepository.class);
+    private Guest universityGuest = new Guest.GuestBuilder()
+            .guestName(guestName)
+            .relatedBy(RelationshipType.UNIVERSITY)
+            .build();
 
 }
