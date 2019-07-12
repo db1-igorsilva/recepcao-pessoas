45b2923ec4e9babda3d497be18b3794f1a93f5d5
db1-igorsilva <igor.silva@db1.com.br>
2019-07-12 12:51:49 -0300

    SecurityConfig permitAll()

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/config/SecurityConfig.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/config/SecurityConfig.java
@@ -2,20 +2,21 @@ package br.com.db1.recepcao.recepcaopessoas.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
                 .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .permitAll();
+        httpSecurity.csrf().disable();
     }
 
     @Autowired
