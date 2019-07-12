65aee207080cbba2f7b52325aef0c1956755e9e9
db1-igorsilva <igor.silva@db1.com.br>
2019-07-12 10:06:33 -0300

    SecurityConfig inMemory

--- src/test/java/br/com/db1/recepcao/recepcaopessoas/config/SecurityConfig.java
+++ src/test/java/br/com/db1/recepcao/recepcaopessoas/config/SecurityConfig.java
@@ -1,11 +1,33 @@
 package br.com.db1.recepcao.recepcaopessoas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authentication) throws Exception {
+        authentication.inMemoryAuthentication()
+                .withUser("igorhenss")
+                .password("passmeby")
+                .roles("USER")
+                .and()
+                .withUser("db1")
+                .password("db1")
+                .roles("USER", "ADMIN");
+    }
 
 }
