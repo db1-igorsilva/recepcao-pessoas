74aed3ad6bc91bf74adc59818ecfdf4f3a5f0054
db1-igorsilva <igor.silva@db1.com.br>
2019-07-12 12:18:29 -0300

    @Repository annotation added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/GuestRepository.java
@@ -3,10 +3,12 @@ package br.com.db1.recepcao.recepcaopessoas.repository;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.RelationshipType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface GuestRepository extends JpaRepository<Guest, UUID> {
 
     Guest findByName(String name);
--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitRepository.java
@@ -3,11 +3,13 @@ package br.com.db1.recepcao.recepcaopessoas.repository;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Guest;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface VisitRepository extends JpaRepository<Visit, UUID> {
 
     List<Visit> findByDate(LocalDate date);
