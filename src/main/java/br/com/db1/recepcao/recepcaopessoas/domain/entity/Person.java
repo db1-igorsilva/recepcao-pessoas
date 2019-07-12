6e08099dc9a9bd3f75456c96d901decebc29cebc
db1-igorsilva <igor.silva@db1.com.br>
2019-07-12 08:43:53 -0300

    Person attributes added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Person.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/Person.java
@@ -1,12 +1,24 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
 @Table(name = "person")
 public class Person {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @Column(name = "cpf", nullable = false)
+    private String cpf;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate data_nascimento;
 
 }
