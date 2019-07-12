4873a8d01b3a57947a2756d2188a9ed711379e5d
db1-igorsilva <igor.silva@db1.com.br>
2019-07-12 08:24:51 -0300

    VisitGuest class created with it's attributes and constructor

---
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/entity/VisitGuest.java
@@ -0,0 +1,24 @@
+package br.com.db1.recepcao.recepcaopessoas.domain.entity;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "visit_guest")
+public class VisitGuest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_guest", nullable = false, referencedColumnName = "id")
+    private Guest guest;
+
+    @ManyToOne
+    @JoinColumn(name = "id_visit", nullable = false, referencedColumnName = "id")
+    private Visit visit;
+
+    public VisitGuest() { }
+
+}
