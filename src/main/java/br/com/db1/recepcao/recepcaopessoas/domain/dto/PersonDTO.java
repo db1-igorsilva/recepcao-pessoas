4e035569b42db7834e2e6ac696f094241cbc8a79
db1-igorsilva <igor.silva@db1.com.br>
2019-07-15 08:25:38 -0300

    Constructor, Getters and Setters done.

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/PersonDTO.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/PersonDTO.java
@@ -10,4 +10,45 @@ public class PersonDTO {
     private String cpf;
     private LocalDate birthDate;
 
+    private PersonDTO(UUID id, String name, String cpf, LocalDate birthDate) {
+        setId(id);
+        setName(name);
+        setCpf(cpf);
+        setBirthDate(birthDate);
+    }
+
+    // GETTERS AND SETTERS
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
 }
