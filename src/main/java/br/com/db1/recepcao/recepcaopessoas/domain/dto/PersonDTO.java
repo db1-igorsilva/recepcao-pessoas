66503c3d8d0aa37e18811ca84e4720878887b4b9
db1-igorsilva <igor.silva@db1.com.br>
2019-07-15 08:22:18 -0300

    Attributes added to PersonDTO.

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/PersonDTO.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/PersonDTO.java
@@ -1,4 +1,13 @@
 package br.com.db1.recepcao.recepcaopessoas.domain.dto;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class PersonDTO {
+
+    private UUID id;
+    private String name;
+    private String cpf;
+    private LocalDate birthDate;
+
 }
