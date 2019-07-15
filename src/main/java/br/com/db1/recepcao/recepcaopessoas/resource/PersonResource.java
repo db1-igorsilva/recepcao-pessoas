758fe6df570d6727748f2a165d5c3a54504f14d6
db1-igorsilva <igor.silva@db1.com.br>
2019-07-15 09:33:25 -0300

    PersonService and PersonResource created.

---
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/PersonResource.java
@@ -0,0 +1,4 @@
+package br.com.db1.recepcao.recepcaopessoas.resource;
+
+public class PersonResource {
+}
---
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/PersonService.java
@@ -0,0 +1,4 @@
+package br.com.db1.recepcao.recepcaopessoas.service;
+
+public class PersonService {
+}
