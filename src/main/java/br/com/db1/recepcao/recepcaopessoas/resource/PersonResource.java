ddc9d45528dffbe5027825c54b9828d0676f0b33
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 14:21:07 -0300

    @RestController and @RequestMapping added to PersonResource

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/PersonResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/PersonResource.java
@@ -1,4 +1,9 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/person")
 public class PersonResource {
 }
