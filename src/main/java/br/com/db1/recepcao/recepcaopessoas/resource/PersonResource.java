abff3fe2ea9a6c5855cdffe15d393f1ffa7d2779
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 14:30:27 -0300

    @Post, @Get and @Delete added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/PersonResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/PersonResource.java
@@ -1,9 +1,33 @@
 package br.com.db1.recepcao.recepcaopessoas.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.PersonDTO;
+import br.com.db1.recepcao.recepcaopessoas.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/person")
 public class PersonResource {
+
+    @Autowired
+    private PersonService personService;
+
+    @PostMapping(value = "/post")
+    public PersonDTO post(@RequestBody PersonDTO body) {
+        return personService.save(body);
+    }
+
+    @GetMapping(value = "/getAll")
+    public List<PersonDTO> getAll() {
+        return personService.getAll();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") UUID id) {
+        personService.delete(id);
+    }
+
 }
