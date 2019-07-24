043bedce74850e7ce4a22f0972c7ca72f29ff5d3
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 15:42:29 -0300

    VisitPersonService done

---
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/service/VisitPersonService.java
@@ -0,0 +1,63 @@
+package br.com.db1.recepcao.recepcaopessoas.service;
+
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.PersonDTO;
+import br.com.db1.recepcao.recepcaopessoas.domain.dto.VisitPersonDTO;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Person;
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.VisitPerson;
+import br.com.db1.recepcao.recepcaopessoas.repository.PersonRepository;
+import br.com.db1.recepcao.recepcaopessoas.repository.VisitPersonRepository;
+import br.com.db1.recepcao.recepcaopessoas.repository.VisitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class VisitPersonService {
+
+    @Autowired
+    private VisitPersonRepository visitPersonRepository;
+
+    @Autowired
+    private VisitRepository visitRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    // CREATE
+
+    public VisitPersonDTO save(VisitPersonDTO dto) {
+        VisitPerson visitPerson = new VisitPerson(personRepository.getOne(UUID.fromString(dto.getPerson())), visitRepository.getOne(UUID.fromString(dto.getVisit())));
+        return visitPersonToDto(visitPersonRepository.save(visitPerson));
+    }
+
+    // READ
+
+    public List<VisitPersonDTO> getAll() {
+        List<VisitPerson> visitPersons = visitPersonRepository.findAll();
+        List<VisitPersonDTO> allVisitPersonDtos = visitPersons.stream()
+                .map(visitPerson -> visitPersonToDto(visitPerson))
+                .collect(Collectors.toList());
+        return allVisitPersonDtos;
+    }
+
+    // UPDATE
+
+    // DELETE
+
+    public void delete(UUID id) {
+        VisitPerson visitPersonToDelete = visitPersonRepository.getOne(id);
+        visitPersonRepository.delete(visitPersonToDelete);
+    }
+
+    // METHODS
+
+    private VisitPersonDTO visitPersonToDto(VisitPerson visitPerson) {
+        return new VisitPersonDTO(visitPerson.getId(),
+                    visitPerson.getPerson().getId().toString(),
+                    visitPerson.getVisit().getId().toString());
+    }
+
+}
