c428362e0e271b66ed843806dcb63df5063fa22c
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 16:55:41 -0300

    All requests with @CrossOrigin

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/resource/VisitResource.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping(value = "/visit")
 public class VisitResource {
@@ -17,36 +16,43 @@ public class VisitResource {
     @Autowired
     private VisitService visitService;
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping(value = "/post")
     public VisitDTO post(@RequestBody VisitDTO body) {
         return visitService.save(body);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/getAll")
     public List<VisitDTO> getAll() {
         return visitService.getAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/getOne/{id}")
     public VisitDTO getOne(@PathVariable("id") UUID id) {
         return visitService.getOne(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/getByDate/{date}")
     public List<VisitDTO> getByDate(@PathVariable("date") LocalDate date) {
         return visitService.getByDate(date);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/getByGuest/{guest}")
     public List<VisitDTO> getByGuest(@PathVariable("guest") UUID id) {
         return visitService.getByGuest(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PutMapping(value = "/{id}")
     public VisitDTO put(@PathVariable("id") UUID id, @RequestBody VisitDTO body) {
         return visitService.put(id, body);
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable("id") UUID id) {
         visitService.delete(id);
