38c19203d5dc805a99f7b7fedef5033748cf4be3
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 15:31:21 -0300

    VisitPersonDTO done

---
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/domain/dto/VisitPersonDTO.java
@@ -0,0 +1,65 @@
+package br.com.db1.recepcao.recepcaopessoas.domain.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public class VisitPersonDTO implements Serializable {
+
+    private UUID id;
+    private String visit;
+    private String person;
+
+    // CONSTRUCTOR
+
+    public VisitPersonDTO(UUID id, String visit, String person) {
+        this.id = id;
+        this.visit = visit;
+        this.person = person;
+    }
+
+    // EQUALS AND HASH
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof VisitPersonDTO)) return false;
+        VisitPersonDTO that = (VisitPersonDTO) o;
+        return getId().equals(that.getId()) &&
+                getVisit().equals(that.getVisit()) &&
+                getPerson().equals(that.getPerson());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+
+    // GETTERS AND SETTERS
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getVisit() {
+        return visit;
+    }
+
+    public void setVisit(String visit) {
+        this.visit = visit;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+}
