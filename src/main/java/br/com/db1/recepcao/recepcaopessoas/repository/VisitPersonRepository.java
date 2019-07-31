ee7644cec20578ccfaed012588833f9ad0d14c1e
Igor <igorhenss@gmail.com>
2019-07-30 21:55:20 -0300

    getByVisit method added

--- src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitPersonRepository.java
+++ src/main/java/br/com/db1/recepcao/recepcaopessoas/repository/VisitPersonRepository.java
@@ -1,11 +1,14 @@
 package br.com.db1.recepcao.recepcaopessoas.repository;
 
+import br.com.db1.recepcao.recepcaopessoas.domain.entity.Visit;
 import br.com.db1.recepcao.recepcaopessoas.domain.entity.VisitPerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VisitPersonRepository extends JpaRepository<VisitPerson, UUID> {
+    List<VisitPerson> getByVisit(Visit visit);
 }
